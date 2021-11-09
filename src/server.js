d09e8becdb7ace9494b3dd99610ffdd2004aada6
Palermo Rodriguez <nicolas.palermo@kcc.com>
2021-11-09 18:32:54 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -33,17 +33,17 @@ const ServerUp = (port = 8090, message= 'The server is ready') =>{
 
     server.use(express.json());
     server.use(express.urlencoded({extended: false}));
-    server.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+    server.use('/documentation', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
     server.use(helmet());
 
     /////////////////////////////////////////////////////////////
     // Routers
 
-    server.use('/api/v1/agendas', agendaRouter());
-    server.use('/api/v1/mediosDePago', paymentMethodRouter());
-    server.use('/api/v1/productos', productRouter());
-    server.use('/api/v1/usuarios', userRouter());
-    server.use('/api/v1/pedidos', orderRouter());
+    server.use('/agendas', agendaRouter());
+    server.use('/mediosDePago', paymentMethodRouter());
+    server.use('/productos', productRouter());
+    server.use('/usuarios', userRouter());
+    server.use('/pedidos', orderRouter());
 
     /////////////////////////////////////////////////////////////
     //Puerto
