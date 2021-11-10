b4d5cfa66a5c1e7e8f8e7afa84e090f4cecaa69b
Palermo Rodriguez <nicolas.palermo@kcc.com>
2021-11-09 21:13:56 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,15 +8,15 @@ module.exports = {
     env_local: {
       "NODE_ENV": "local",
       "API_DESCRIPTION": "Estás ejecutando tu API en modo desarrollador.",
-      "PORT": process.env.PORT,
-      "DB_HOST": process.env.DB_HOST,
-      "DB_USERNAME": process.env.DB_USERNAME,
-      "DB_PASSWORD": process.env.DB_PASSWORD,
-      "DB_NAME": process.env.DB_NAME,
-      "DB_PORT": process.env.DB_PORT,
-      "REDIS_HOST": process.env.REDIS_HOST,
-      "REDIS_PORT": process.env.REDIS_PORT,
-      "JWT_PASS": process.env.JWT_PASS
+      "PORT"=3000,
+      "DB_HOST"="sprint3.cxbgbiilntg1.sa-east-1.rds.amazonaws.com",
+      "DB_USERNAME"="rootnico",
+      "DB_PASSWORD"=12345678,
+      "DB_NAME"="sprint3",
+      "DB_PORT"=3306,
+      "REDIS_HOST"="redis-sprint3.cwkyre.0001.sae1.cache.amazonaws.com",
+      "REDIS_PORT"=6379,
+      "JWT_PASS"=1234567890
     },
     env_production: {
       "NODE_ENV": "production",
